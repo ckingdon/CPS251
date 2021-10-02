c92bc655bba6668d1dd38be8c5d846cf371e86b9
ckingdon <ckingdon@users.noreply.github.com>
2021-10-02 11:55:51 -0400

    W05 Layout Project

--- AndroidSample/app/src/main/java/com/example/androidsample/MainActivity.kt
+++ AndroidSample/app/src/main/java/com/example/androidsample/MainActivity.kt
@@ -4,27 +4,36 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 
-import kotlinx.android.synthetic.main.activity_main.*
+//import kotlinx.android.synthetic.main.activity_main.*
+import com.example.androidsample.databinding.ActivityMainBinding // added CK 2021-09-17
+
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     fun convertCurrency(view: View) {
-        if (dollarText.text.isNotEmpty()) {
-            val dollarValue = dollarText.text.toString().toFloat()
+        //if (dollarText.text.isNotEmpty()) {
+        if (binding.dollarText.text.isNotEmpty()) {
+            //val dollarValue = dollarText.text.toString().toFloat()
+            val dollarValue = binding.dollarText.text.toString().toFloat()
+            //val scott = dollarText.text.toString()
             val euroValue = dollarValue * 0.85f
-            textView.text = euroValue.toString()
+            //textView.text = euroValue.toString()
+            binding.textView.text = euroValue.toString()
             //textView.text = "%.2f".format(euroValue.toString())
             //dollarText.text = euroValue.toString()
         } else {
-            textView.text = getString(R.string.no_value_string)
+            //textView.text = getString(R.string.no_value_string)
+            binding.textView.text = getString(R.string.no_value_string)
             //dollarText.text = getString(R.string.no_value_string)
-
         }
     }
-
 }
